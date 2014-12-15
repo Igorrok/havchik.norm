6b48da281af77461ea6797ef8faff6a6eb6c17ad
Pavlo Okhrem <on-line@i.ua>
2014-12-15 15:51:31 +0200

    Uploading db sources again

--- www/Database_source/magento.sql
+++ www/Database_source/magento.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Дек 10 2014 г., 13:53
--- Версия сервера: 5.5.25
--- Версия PHP: 5.3.13
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
